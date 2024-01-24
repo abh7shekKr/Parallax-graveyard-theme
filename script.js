84f58ff44505fe2b4b2971a2e69327ddb749a2a8
abh7shekKr <abhi.bs.0001@gmail.com>
2024-01-25 01:51:56 +0530

    js updated

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.addEventListener("scroll", ()=>{
 
     text.style.marginTop = value * 2.5 + 'px';
     treeLeft.style.left = value * -1.5 + 'px';
-    treeRight.style.left = value * -1.5 + 'px';
+    treeRight.style.left = value * 1.5 + 'px';
     gateLeft.style.left = value * 0.5 + 'px';
     gateRight.style.left = value * -0.5 + 'px';
 })
